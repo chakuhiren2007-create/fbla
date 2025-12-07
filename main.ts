5d52d8f82a3d165da7ac651f1092fa0012b2245f
chakuhiren2007-create <chakuhiren2007@gmail.com>
2025-12-07 11:39:29 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+let _67gooningmaster = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f 3 3 3 3 3 3 2 e e f . . 
+    . . 3 3 2 3 3 3 3 3 3 2 e f . . 
+    . 3 f f f 3 e e 3 3 3 f f f . . 
+    3 f f e f 3 3 4 4 3 3 3 e f f . 
+    . f e e 4 1 3 3 3 3 1 3 3 e f . 
+    . . f e e d d d d d d e 3 f . . 
+    . . . f e e 4 4 4 4 e e 3 . . . 
+    . . e 4 f 2 2 2 2 2 2 3 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(_67gooningmaster)
